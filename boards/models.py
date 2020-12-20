59f3e96a60f0a343006de3dbbb1e5ece18313bd7
Hahm <hsh07260@naver.com>
2020-12-20 17:36:31 +0900

    update: verbose_name 추가

--- boards/models.py
+++ boards/models.py
@@ -3,13 +3,19 @@ from core import models as core_models
 
 
 class Board(core_models.TimeStampedModel):
+    postNo = models.CharField(max_length=8, blank=True, verbose_name="게시물 번호")
+
     author = models.ForeignKey(
         "users.User",
         related_name="boards",
         on_delete=models.CASCADE,
+        verbose_name="작성자",
     )
-    title = models.CharField(max_length=200, blank=True)
-    board_num = models.CharField(max_length=8, blank=True)
+    title = models.CharField(max_length=200, blank=True, verbose_name="제목")
+    contents = models.TextField(blank=True, verbose_name="내용")
 
-    view_cnts = models.PositiveIntegerField()
-    contents = models.TextField(blank=True)
+    viewCnts = models.PositiveIntegerField(default=0, verbose_name="조회수")
+
+    class Meta:
+        verbose_name = "공지사항"
+        verbose_name_plural = "공지사항"
