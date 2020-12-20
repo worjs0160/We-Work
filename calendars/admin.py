e6ee919529afb732f2ccc02b72e6ff503f55a987
unknown <worjs0160@naver.com>
2020-12-21 01:52:01 +0900

    calendar make

--- calendars/admin.py
+++ calendars/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+
+@admin.register(models.CalendarType)
+class TypeAdmin(admin.ModelAdmin):
+
+    """ Type Admin Difinition """
+
+    list_display = ("type_form", "used_by")
+
+    def used_by(self, obj):
+        return obj.calendars.count()
+
+
+@admin.register(models.Calendar)
+class CustomUserAdmin(admin.ModelAdmin):
+
+    """ Custom Calendar Admin Difinition """
+
+    list_display = (
+        "host",
+        "category",
+        "start_time",
+        "end_time",
+        "title",
+        "place",
+        "schedule",
+        "attached_file",
+    )
--- calendars/models.py
+++ calendars/models.py
@@ -1,3 +1,52 @@
 from django.db import models
+from core import models as core_models
 
-# Create your models here.
+
+class AbstractItem(core_models.TimeStampedModel):
+
+    """ Abstract Item """
+
+    type_form = models.CharField(max_length=10)
+
+    class Meta:
+        abstract = True
+
+    def __str__(self):
+        return self.type_form
+
+
+class CalendarType(AbstractItem):
+
+    """ Calendar Type Model Definition """
+
+    class Meta:
+        verbose_name = "Calendar Type"
+        ordering = ["created"]
+
+
+class File(core_models.TimeStampedModel):
+
+    """ File Model Definition """
+
+    pass
+
+
+class Calendar(core_models.TimeStampedModel):
+
+    """ Calenadar Model Definition """
+
+    host = models.ForeignKey(
+        "users.User", related_name="calendars", on_delete=models.CASCADE
+    )
+    category = models.ForeignKey(
+        "calendars.CalendarType", related_name="calendars", on_delete=models.CASCADE
+    )
+    start_time = models.DateField()
+    end_time = models.DateField()
+    title = models.CharField(max_length=50)
+    place = models.CharField(max_length=50)
+    schedule = models.CharField(max_length=200, blank=True)
+    attached_file = models.FileField(blank=True)
+
+    def __str__(self):
+        return self.name
