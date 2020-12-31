3356f97d531358e6029e07aafb2f93f86cc9fc18
skddls88 <51692156+skddls88@users.noreply.github.com>
2020-12-31 20:58:30 +0900

    Revert "템플릿 적용"
    This reverts commit dea1a8a7919e7c2b4e4a7bc12f8d637e5238ba45.

--- boards/models.py
+++ boards/models.py
@@ -1,9 +1,22 @@
 from django.db import models
+from django.shortcuts import reverse
+from django.core.validators import MinLengthValidator, MaxLengthValidator
+from ckeditor_uploader.fields import RichTextUploadingField
 from core import models as core_models
+
+title_MinLenValidator = MinLengthValidator(2, "2자 이상 입력해주세요.(2자 ~ 80자 이내)")
+title_MaxLenValidator = MaxLengthValidator(80, "80자 이내로 입력해주세요.(2자 ~ 80자 이내)")
+contents_MinLenValidator = MinLengthValidator(10, "글이 너무 짧습니다. 10자 이상 입력해주세요.")
 
 
 class Board(core_models.TimeStampedModel):
-    postNo = models.CharField(max_length=8, blank=True, verbose_name="게시물 번호")
+    class Meta:
+        verbose_name = "공지사항"
+        verbose_name_plural = "공지사항"
+
+    postNo = models.AutoField(
+        auto_created=True, primary_key=True, serialize=False, verbose_name="게시물 번호"
+    )
 
     author = models.ForeignKey(
         "users.User",
@@ -11,11 +24,22 @@ class Board(core_models.TimeStampedModel):
         on_delete=models.CASCADE,
         verbose_name="작성자",
     )
-    title = models.CharField(max_length=200, blank=True, verbose_name="제목")
-    contents = models.TextField(blank=True, verbose_name="내용")
+    title = models.CharField(
+        max_length=80,
+        validators=[title_MaxLenValidator, title_MinLenValidator],
+        verbose_name="제목",
+    )
+    contents = RichTextUploadingField(
+        validators=[contents_MinLenValidator], verbose_name="내용"
+    )
 
     viewCnts = models.PositiveIntegerField(default=0, verbose_name="조회수")
 
-    class Meta:
-        verbose_name = "공지사항"
-        verbose_name_plural = "공지사항"
+    def get_absolute_url(self):
+        return reverse("boards:board_detail", kwargs={"pk": self.pk})
+
+    @property
+    def update_viewCnts(self):
+        self.viewCnts += 1
+        self.save()
+        return self.viewCnts
--- config/urls.py
+++ config/urls.py
@@ -14,6 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path, include
 
 urlpatterns = [
@@ -21,4 +23,7 @@ urlpatterns = [
     path("users/", include("users.urls", namespace="users")),
     path("boards/", include("boards.urls", namespace="boards")),
     path("admin/", admin.site.urls),
+    path("ckeditor/", include("ckeditor_uploader.urls")),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
