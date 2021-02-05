d14e1ae009605979519f1e44f638a84446b14120
soy567 <soy567@naver.com>
2021-02-05 15:34:45 +0900

    결재 models 수정

--- approvals/models.py
+++ approvals/models.py
@@ -1,12 +1,34 @@
 from django.db import models
-from core.models import TimeStampedModel, title_MaxLenValidator, title_MinLenValidator, contents_MinLenValidator
+from core.models import (
+    TimeStampedModel,
+    title_MaxLenValidator,
+    title_MinLenValidator,
+    contents_MinLenValidator,
+)
+
 # Create your models here.
+
 
 class DocBase(TimeStampedModel):
     """
-        결재서류 Base 클래스(공통내용)
+    결재서류 Base 클래스(공통내용)
     """
-    author = models.ForeignKey("users.User", related_name="%(class)s".lower(), on_delete=models.CASCADE)
+
+    STA_REQ = "reqest"
+    STA_REV = "review"
+    STA_APP = "approved"
+    STA_RET = "return"
+
+    STA_CHOICES = (
+        (STA_REQ, "결재전"),
+        (STA_REV, "결재중"),
+        (STA_APP, "결재완료"),
+        (STA_RET, "결재반려"),
+    )
+
+    author = models.ForeignKey(
+        "users.User", related_name="%(class)s".lower(), on_delete=models.CASCADE
+    )
     title = models.CharField(
         max_length=80,
         validators=[title_MaxLenValidator, title_MinLenValidator],
@@ -14,6 +36,17 @@ class DocBase(TimeStampedModel):
     )
     contents = models.TextField(
         validators=[contents_MinLenValidator], verbose_name="내용"
+    )
+
+    viewer = models.ManyToManyField(
+        "users.User",
+        related_name="%(class)s".lower() + "_viewer",
+        blank=True,
+        verbose_name="열람자",
+    )
+
+    status = models.CharField(
+        choices=STA_CHOICES, max_length=10, default=STA_REQ, verbose_name="결재상황"
     )
 
     class Meta:
@@ -24,13 +57,14 @@ class Draft(DocBase):
     """
     기안서 클래스
     """
-    
+
     class Meta:
         verbose_name = "기안서"
         verbose_name_plural = "기안서"
 
     pass
-    
+
+
 class Meeting(DocBase):
     """
     회의보고서
@@ -39,35 +73,41 @@ class Meeting(DocBase):
     class Meta:
         verbose_name = "회의보고서"
         verbose_name_plural = "회의보고서"
+
     pass
+
 
 class Business(DocBase):
     """
     업무보고서
     """
+
     class Meta:
         verbose_name = "업무보고서"
         verbose_name_plural = "업무보고서"
 
     pass
 
+
 class Result(DocBase):
     """
     결과보고서
     """
+
     class Meta:
         verbose_name = "결과보고서"
         verbose_name_plural = "결과보고서"
 
     pass
 
+
 class Voucher(DocBase):
     """
     지출결의서
     """
+
     class Meta:
         verbose_name = "지출결의서"
         verbose_name_plural = "지출결의서"
-        
-    pass
 
+    pass
