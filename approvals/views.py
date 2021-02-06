6c1f1db8b04b67fcbb871e7c9215d8a259586b87
Hahm <hsh07260@naver.com>
2021-02-06 16:09:40 +0900

    New: 결재 양식별 form 처리

--- approvals/views.py
+++ approvals/views.py
@@ -11,4 +11,19 @@ def doc_format(request, doc):
     doc의 string 인자로 파일 이름값을 받아
     approvals/doc_format/ 에 있는 해당 doc를 render로 반환
     """
-    return render(request, "approvals/doc_format/"+doc)
+    if doc=="draft.html":
+        form = DraftForm()
+
+    elif doc=="voucher.html":
+        form = VoucherForm()
+
+    elif doc=="result.html":
+        form = ResultForm()
+
+    elif doc=="meeting.html":
+        form = MeetingForm()
+
+    elif doc=="business.html":
+        form = Business.html
+
+    return render(request, "approvals/doc_format/"+doc, {"form": form})
