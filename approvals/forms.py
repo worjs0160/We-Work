576ed2d73a08edafaeda198b0eb7adbd5ca29286
soy567 <soy567@naver.com>
2021-02-06 16:18:30 +0900

    결재 폼 생성

---
+++ approvals/forms.py
@@ -0,0 +1,34 @@
+from django import forms
+from . import models
+
+base_filds = ("author", "title", "contents", "viewer")
+
+
+class DratfForm(forms.Form):
+    class Meta:
+        model = models.Draft
+        fields = base_filds
+
+
+class MeetingForm(forms.Form):
+    class Meta:
+        model = models.Meeting
+        fields = base_filds
+
+
+class BusinessForm(forms.Form):
+    class Meta:
+        model = models.Business
+        fields = base_filds
+
+
+class ResultForm(forms.Form):
+    class Meta:
+        model = models.Result
+        fields = base_filds
+
+
+class VoucherForm(forms.Form):
+    class Meta:
+        model = models.Voucher
+        fields = base_filds
